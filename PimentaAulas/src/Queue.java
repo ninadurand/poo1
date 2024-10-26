704102e6edf18d6c43269ea8997e704510313a36
Marina Mendes Durand <133005002+ninadurand@users.noreply.github.com>
2024-10-25 23:35:50 -0300

    Add files via upload

---
+++ PimentaAulas/src/Queue.java
@@ -0,0 +1,35 @@
+public class Queue {
+    
+    private int valor;
+    private Queue proxQueue;
+    private Queue antQueue;
+
+    public boolean isEmpty(){
+        
+         return boolean;
+    }
+
+    public void enqueue(int valor){
+
+    }
+    public int getValor() {
+        return valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    public Queue getProxQueue() {
+        return proxQueue;
+    }
+    public void setProxQueue(Queue proxQueue) {
+        this.proxQueue = proxQueue;
+    }
+    public Queue getAntQueue() {
+        return antQueue;
+    }
+    public void setAntQueue(Queue antQueue) {
+        this.antQueue = antQueue;
+    }
+
+
+}
